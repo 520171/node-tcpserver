181a60256165527ba9c3d77c62cffaeaf981e640
JackChen <309581454@qq.com>
2018-12-24 17:35:12 +0800

    tcp server、websocket server更新

--- websocket.js
+++ websocket.js
@@ -27,7 +27,23 @@ function httpHandler(req, res) {
 http.listen(http_port);
 console.info("HTTP server listening on " + http_port);
 
-io.sockets.on('connection', function (socket) {
+// middleware，token验证
+io.use(function (socket, next) {
+    var token = socket.handshake.query.token;
+
+    if (typeof token == 'undefined' || token == null || token == '') {
+        console.error('Authentication  error');
+        socket.disconnect();
+        next(new Error("Socket auth error: No token provided"));
+    } else {
+        return next();
+    }
+});
+
+io.on('connection', function (socket) {
+    var token = socket.handshake.query.token;
+    console.log(token);
+
     var addUser = false;
 
     var tcpClient = new net.Socket();
@@ -39,21 +55,22 @@ io.sockets.on('connection', function (socket) {
 
         //tcp客户端接收消息
         tcpClient.on('data', function (data) {
-            console.log(data.toString());
+            // console.log(data.toString());
             socket.emit("receive tcp", data.toString());
         });
 
         tcpClient.on('end', function (data) {
-            console.log('END DATA : ' + data);
+            console.log('客户端[' + socket.username + ']与tcp server断开连接');
         });
     });
 
     socket.on('receive', function (data) {
         console.log('浏览器【' + socket.username + '】：' + data);
-        socket.emit('receive websocket', {
-            username: socket.username,
-            message: data
-        });
+        ////websocket服务器回复给浏览器端，暂时屏蔽
+        // socket.emit('receive websocket', {
+        //     username: socket.username,
+        //     message: data
+        // });
         if (tcpClient) {
             tcpClient.write('【' + socket.username + '】：' + data);
         }
@@ -74,8 +91,9 @@ io.sockets.on('connection', function (socket) {
     });
 
     socket.on('disconnect', function () {
+        tcpClient.end();
         clients.splice(clients.indexOf(socket), 1);
-        console.log('客户端[' + socket.username + ']已离开,当前在线人数：' + clients.length);
+        console.log('客户端[' + socket.username + ']已断开连接,当前在线人数：' + clients.length);
         socket.emit('logout', {
             username: socket.username,
             onlineUsers: clients.length
